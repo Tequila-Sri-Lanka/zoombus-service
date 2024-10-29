2a1fc9fca6ff3a1ef159adfde70ee175ca92eda4
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-10-29 10:55:30 +0530

    Initial commit: setup basic project structure

---
+++ src/main/java/zoombus/ZoomBusApplication.java
@@ -0,0 +1,13 @@
+package zoombus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoomBusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zoombus/ZoomBusApplicationTests.java
@@ -0,0 +1,13 @@
+package zoombus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomBusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
